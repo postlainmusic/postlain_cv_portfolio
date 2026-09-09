7ef980735b718298d0fa9a8d66b7a8e5a44c5ee1
postlainmusic <postlain.music@gmail.com>
2026-09-09 17:07:33 +0700

    fix: wire narrative experience into app

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,22 +1,12 @@
 import React, { useEffect } from 'react';
 import { useAppStore } from './stores/useAppStore';
 import { getContent } from './content';
-import { useActiveSection } from './hooks/useActiveSection';
-import { Layout } from './core/Layout';
-import { Chapter00Overture } from './sections/Chapter00Overture';
-import { Chapter01Orchestration } from './sections/Chapter01Orchestration';
-import { Chapter02SonicSpace } from './sections/Chapter02SonicSpace';
-import { Chapter03Matrix } from './sections/Chapter03Matrix';
-import { Chapter04Transmission } from './sections/Chapter04Transmission';
-
-const SECTION_IDS = ['overture', 'orchestration', 'sonic-space', 'matrix', 'transmission'];
+import { NarrativeExperience } from './NarrativeExperience';
 
 export const App: React.FC = () => {
   const { locale } = useAppStore();
   const content = getContent(locale);
-  const activeSection = useActiveSection(SECTION_IDS);
 
-  // Sync document title and language tag
   useEffect(() => {
     document.title = content.meta.documentTitle;
     document.documentElement.lang = locale;
@@ -26,15 +16,7 @@ export const App: React.FC = () => {
     }
   }, [content, locale]);
 
-  return (
-    <Layout content={content} activeSection={activeSection}>
-      <Chapter00Overture content={content.chapter00} />
-      <Chapter01Orchestration content={content.chapter01} />
-      <Chapter02SonicSpace content={content.chapter02} />
-      <Chapter03Matrix content={content.chapter03} />
-      <Chapter04Transmission content={content.chapter04} />
-    </Layout>
-  );
+  return <NarrativeExperience />;
 };
 
 export default App;
